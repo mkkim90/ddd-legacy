4a8fd800a3ef1f370e9121bdef463f37daddaacd
mkkim90 <mkzzang0928@gmail.com>
2025-01-27 11:45:34 +0900

    refactor : 구분자 상수처리

--- src/main/java/stringcalculator/StringCalculator.kt
+++ src/main/java/stringcalculator/StringCalculator.kt
@@ -4,18 +4,27 @@ class StringCalculator {
     fun calculate(text: String?): Int {
         if (text.isNullOrBlank()) return 0
         return text.parseDelimiter().sumOf {
-            val number= it.toIntOrNull() ?: throw RuntimeException("숫자가 아닌 문자열 $it")
-            if (number < 0) throw RuntimeException("음수 허용하지 않습니다 $it")
-            number
+            toInt(it)
         }
     }
 
     private fun String.parseDelimiter(): List<String> {
-        val regex = Regex("//(.)\n(.*)")
+        val regex = Regex(CUSTOM_DELIMITER_PATTERN)
         val matchResult = regex.find(this)
         return matchResult?.let {
             val (customDelimiter, numbers) = it.destructured
             numbers.split(customDelimiter)
-        } ?: this.split(",", ":")
+        } ?: this.split(*DEFAULT_DELIMITERS.toCharArray())
+    }
+
+    private fun toInt(it: String): Int {
+        val number = it.toIntOrNull() ?: throw RuntimeException("숫자가 아닌 문자열 $it")
+        if (number < 0) throw RuntimeException("음수 허용하지 않습니다 $it")
+        return number
+    }
+
+    companion object {
+        private const val CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)"
+        private const val DEFAULT_DELIMITERS = ";:"
     }
 }
