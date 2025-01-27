6a2d122ea6d1d2405b2e98fd5eb1941e509b2bd1
mkkim90 <mkzzang0928@gmail.com>
2025-01-27 21:35:02 +0900

    refactor : 명시적인 클래스 exception

--- src/main/java/stringcalculator/StringNumbers.kt
+++ src/main/java/stringcalculator/StringNumbers.kt
@@ -1,5 +1,7 @@
 package stringcalculator
 
+import java.lang.IllegalArgumentException
+
 data class StringNumbers(
     private val numbers: List<StringNumber>
 ) {
@@ -19,8 +21,8 @@ data class StringNumber(
 ) {
     companion object {
         fun from(parsedText: String): StringNumber {
-            val number = parsedText.toIntOrNull() ?: throw RuntimeException("숫자가 아닌 문자열 $parsedText")
-            if (number < 0) throw RuntimeException("음수 허용하지 않습니다 $parsedText")
+            val number = parsedText.toIntOrNull() ?: throw NumberFormatException("숫자가 아닌 문자열 $parsedText")
+            if (number < 0) throw IllegalArgumentException("음수 허용하지 않습니다 $parsedText")
             return StringNumber(number = number)
         }
     }
